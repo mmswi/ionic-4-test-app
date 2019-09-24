a2dfc245eca2d3a9e3b6dec7245385756bcc7d4f
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-24 21:02:04 +0300

    Added auth guard in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./guards/auth.guard";
 
 const routes: Routes = [
   {
@@ -9,11 +10,15 @@ const routes: Routes = [
   },
   {
     path: "home",
-    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
+    loadChildren: () =>
+      import("./home/home.module").then(m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: "list",
-    loadChildren: () => import("./list/list.module").then(m => m.ListPageModule)
+    loadChildren: () =>
+      import("./list/list.module").then(m => m.ListPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: "login",
