6354af48a084fc5184556d4089c5abb646d9006c
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-24 20:43:54 +0300

    Adding loginUser method in login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "./../providers/auth-service.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  loginUser() {
+    this.authService.login();
   }
 
+  ngOnInit() {}
 }
