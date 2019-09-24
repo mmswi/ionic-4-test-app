5c125b37b2e0092f5b3a87807d0d421766bbd40e
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-24 20:29:41 +0300

    Added login, logout, isAuthenticated methods in auth-service

--- src/app/providers/auth-service.service.ts
+++ src/app/providers/auth-service.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthServiceService {
+  private isLoggedIn = false;
 
-  constructor() { }
+  constructor() {}
+
+  login(): void {
+    this.isLoggedIn = true;
+  }
+
+  logout(): void {
+    this.isLoggedIn = false;
+  }
+
+  isAuthenticated(): boolean {
+    return this.isLoggedIn;
+  }
 }
