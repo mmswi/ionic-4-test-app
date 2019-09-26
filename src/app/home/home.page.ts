dced9960a08329bbdcc5e06025662d82a124b8db
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-26 23:30:16 +0300

    Getting data from resolved to home component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  users: any;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
+  ngOnInit(): void {
+    if (this.route.snapshot.data["routeData"]) {
+      this.users = this.route.snapshot.data["routeData"];
+      console.log("this.users: ", this.users);
+    }
+  }
 }
