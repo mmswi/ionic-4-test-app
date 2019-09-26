62f1313950a1bffbab93e1c56d509c86cbf4b472
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-26 22:59:07 +0300

    Changed star-wars service to user service - using new url - added type checking - added http headers - added error handling

---
+++ src/app/providers/users.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+import { Users } from "./helpers/users.interface";
+import { UserModel } from "./helpers/users.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class UsersService {
+  private url = "https://jsonplaceholder.typicode.com/users";
+  httpOptions = {
+    headers: new HttpHeaders({ Accept: "application/json" })
+  };
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<Users[]> {
+    return this.http.get<Users[]>(this.url, this.httpOptions).pipe(
+      map(response => {
+        return response.map(user => new UserModel(user));
+      }),
+      catchError(this.handleError<Users[]>("getUsers", []))
+    );
+  }
+
+  getUserById(id: number) {
+    return this.http
+      .get(`${this.url}/${id}`)
+      .pipe(catchError(this.handleError<any>(`getUser by id ${id}`, [])));
+  }
+}
