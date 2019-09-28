df9a108876b6afdc9b5fceb7f79a11466d21c07b
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 13:59:00 +0300

    Taking state on the details page from the route

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { UsersService } from "./../providers/users.service";
 
 @Component({
@@ -12,13 +12,28 @@ export class DetailsPage implements OnInit {
   public errorMessage: any = null;
   constructor(
     private userService: UsersService,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    // using passed state from the router
+    // Note - this won't work on page refresh. For this, a service would be a better option
+    this.route.queryParams.subscribe(() => {
+      const routeState = this.router.getCurrentNavigation().extras.state;
+      if (routeState) {
+        this.user = { picture: routeState.userPicture };
+      }
+    });
+  }
 
   ngOnInit() {
+    // getting params from the route params
     const id = +this.route.snapshot.paramMap.get("id");
-    this.userService
-      .getUserById(id)
-      .subscribe(data => (this.user = data), err => (this.errorMessage = err));
+    this.userService.getUserById(id).subscribe(
+      data => {
+        // merging the current user with the picture prop and received data
+        this.user = { ...this.user, ...data };
+      },
+      err => (this.errorMessage = err)
+    );
   }
 }
