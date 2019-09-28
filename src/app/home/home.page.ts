2b9936ef2ac6d5430dd3f7074c9a098d52dea57e
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 13:40:25 +0300

    Add handleUserClick method on homepage component: - added platform from ionic/angular - added types for methods - added comments - implement forgotten OnInit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { userInfo } from "os";
+import { ActivatedRoute, Router, NavigationExtras } from "@angular/router";
+import { Platform } from "@ionic/angular";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
   users: any;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private platform: Platform
+  ) {}
 
-  getRandomPictureName() {
+  getRandomPictureName(): String {
     // returns a random picture name
     return `picture-${Math.floor(Math.random() * 6) + 1}.jpg`;
+  }
+
+  // currently handles only href detection
+  // for other buttons on the card, I can use other methods with $event.stopPropagation()
+  // to prevend handleUserClick calling
+  handleUserClick(target: EventTarget, payload): void {
+    console.log("user clickeddd");
+    //  handle the external link in the ionic card
+    if (target["href"]) {
+      if (this.platform.is("cordova")) {
+        // here we can use the cordova inappBrowser to open the link
+        // - not gonna implement for this example
+      } else {
+        window.open(target["href"], "_blank");
+      }
+    } else {
+      // if the user clicks on the card, navigate inside the app
+      const { id, picture } = payload;
+      // using NavigationExtras to pass data between views
+      const navigationState: NavigationExtras = {
+        state: {
+          userPicture: picture
+        }
+      };
+      // actual navigation
+      this.router.navigate([`details/${id}`], navigationState);
+    }
   }
 
   ngOnInit(): void {
