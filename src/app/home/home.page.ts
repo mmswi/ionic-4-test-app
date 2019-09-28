301fb331bf1d89fc461efd5699d80e6e2c5e992d
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 10:56:43 +0300

    Created a getRandomPictureName method

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,11 @@ export class HomePage {
 
   constructor(private route: ActivatedRoute, private router: Router) {}
 
+  getRandomPictureName() {
+    // returns a random picture name
+    return `picture-${Math.floor(Math.random() * 6) + 1}`;
+  }
+
   ngOnInit(): void {
     if (this.route.snapshot.data["routeData"]) {
       this.users = this.route.snapshot.data["routeData"];
