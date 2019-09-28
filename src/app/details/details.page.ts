a6ce2ce3a687d5b3d77e00b74dc95c982d223b1c
Mihai Marinescu <marinescu.mihai88@gmail.com>
2019-09-28 09:59:56 +0300

    Making getUserById call in the details component

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { UsersService } from "./../providers/users.service";
 
 @Component({
   selector: "app-list",
@@ -6,33 +8,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["details.page.scss"]
 })
 export class DetailsPage implements OnInit {
-  private selectedItem: any;
-  private icons = [
-    "flask",
-    "wifi",
-    "beer",
-    "football",
-    "basketball",
-    "paper-plane",
-    "american-football",
-    "boat",
-    "bluetooth",
-    "build"
-  ];
-  public items: Array<{ title: string; note: string; icon: string }> = [];
-  constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: "Item " + i,
-        note: "This is item #" + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
+  public user: any;
+  public errorMessage: any = null;
+  constructor(
+    private userService: UsersService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
-  // add back when alpha.4 is out
-  // navigate(item) {
-  //   this.router.navigate(['/list', JSON.stringify(item)]);
-  // }
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.userService
+      .getUserById(id)
+      .subscribe(data => (this.user = data), err => (this.errorMessage = err));
+  }
 }
